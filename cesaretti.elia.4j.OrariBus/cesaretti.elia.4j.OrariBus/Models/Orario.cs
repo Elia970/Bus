b35b471e930d4b61ada6a7b466d31a83ffa884c2
Elia <cesaretti440@gmail.com>
2021-02-27 10:45:05 +0100

    OrariBus

---
+++ cesaretti.elia.4j.OrariBus/cesaretti.elia.4j.OrariBus/Models/Orario.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cesaretti.elia._4j.OrariBus.Models
+{
+    class Orario
+    {
+        private int _ore;
+        private int _minuti;
+        public int ore { get; set; }
+        public int minuti
+        {
+            get
+            {
+                return _minuti;
+            }
+            set
+            {
+                if (value >= 60)
+                {
+                    _minuti = value - 60;
+                    _ore++;
+                }
+                else
+                {
+                    _minuti = value;
+                }
+            }
+        }
+        public Orario()
+        {
+            _ore = 0;
+            _minuti = 0;
+        }
+
+        public Orario(int o, int m)
+        {
+            _ore = o;
+            _minuti = m;
+        }
+        public int GetMinuti()
+        {
+            return minuti;
+        }
+        public void SetMinuti(int m)
+        {
+            /*if (m >= 60)
+            {
+                minuti = m - 60;
+                ore++;
+            }
+            else
+            {
+                minuti = m;
+            }*/
+        }
+        public int GetOre()
+        {
+            return ore;
+        }
+        public void SetOre(int o)
+        {
+            ore = o % 24;
+        }
+        public string Visualizza()
+        {
+            string o = "";
+            o = $"{_ore}:{_minuti}";
+            return o;
+        }
+    }
+}
---
+++ cesaretti.elia.4j.OrariBus/cesaretti.elia.4j.OrariBus/Program.cs
@@ -0,0 +1,24 @@
+﻿using cesaretti.elia._4j.OrariBus.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cesaretti.elia._4j.OrariBus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int f = 0;
+            Orario Partenza170 = new Orario(6, 21);
+            Console.WriteLine("Orario partenza: " + Partenza170.Visualizza());
+            Partenza170.minuti = Partenza170.minuti + 40;
+            Console.WriteLine("Orario partenza: " + Partenza170.Visualizza());
+            Console.ReadLine();
+            f = Partenza170.GetMinuti();
+            Console.WriteLine(f);
+        }
+    }
+}
